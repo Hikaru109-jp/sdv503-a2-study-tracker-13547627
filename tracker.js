d41967b1e1d097b8147be616253551a0ecca2f34
岡野陽光 <hikaru@okanoyoukounoMacBook-Air.local>
2026-05-13 13:10:39 +1200

    add input gathering from user for topic and duration

--- tracker.js
+++ tracker.js
@@ -1,4 +1,24 @@
-let topic = "";
-let duration = 0;
-let totalDuration = 0;
+import readline from "readline";
+
 let sessions = [];
+let totalDuration = 0;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(q) {
+    return new Promise(resolve => rl.question(q, resolve));
+}
+
+async function tracker() {
+    const topic = await ask("Enter topics: ");
+    const duration = await ask("Enter duration: ");
+
+    console.log(`Topic: ${topic} Duration: ${duration}`);
+
+    rl.close();
+}
+
+tracker();
